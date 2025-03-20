e27889dd13c1e966da70977ddcadfc13c94c8835
SamSidd <szs0421@auburn.edu>
2025-03-20 05:35:21 -0500

    Commented

--- modules/scheduler/jobCreationUtility.c
+++ modules/scheduler/jobCreationUtility.c
@@ -5,27 +5,27 @@ void extractArgs(JobPtr job, char* cmdV[]) {
     int i = RUN_JOBNAME_INDEX;
     int countArgs = 0;
     
-    while (cmdV[i] != NULL) {
-        countArgs++;
-        i++;
+    while (cmdV[i] != NULL) {                   // While there's a value in inputBufferArr
+        countArgs++;                            // After the job_name index
+        i++;                                    // Count the number of args
     }
 
     job->jobArgs = (char**)malloc(sizeof(char*) * (countArgs + 1));
-    if (job->jobArgs == NULL) return;
+    if (job->jobArgs == NULL) return;           // Check for error
 
     i = RUN_JOBNAME_INDEX;
-    for (int j = 0; j < countArgs; j++) {
-        job->jobArgs[j] = strdup(cmdV[i]);
-        i++;
+    for (int j = 0; j < countArgs; j++) {       // Loop through and 
+        job->jobArgs[j] = strdup(cmdV[i]);      // create a copy of cmdV's char* string
+        i++;                                    // Also move through the cmdV itself
     }
-    job->jobArgs[countArgs] = NULL;
+    job->jobArgs[countArgs] = NULL;             // Don't forget the null terminator at the end of any array
     
     job->nArgs = countArgs + 1;
     
 }
 
 void populateJobFromBuffer(JobPtr job, char* cmdV[]) {
-    snprintf(job->jobId, JOB_ID_SIZE, "%s", gen_uuid(JOB_ID_SIZE));
+    snprintf(job->jobId, JOB_ID_SIZE, "%s", gen_uuid(JOB_ID_SIZE));     // Copy the job ID
     updateJobName(job, cmdV[RUN_JOBNAME_INDEX]);
     updateJobStatus(job, TO_STRING(create));
     extractArgs(job, cmdV);
@@ -44,6 +44,6 @@ JobPtr createJob(char* cmdV[]) {
 }
 
 void timeStampNewJob(JobPtr job) {
-    job->arrivalTime = time(NULL);
-    job->arrivalTimeStamp = *localtime(&job->arrivalTime);
+    job->arrivalTime = time(NULL);                              // Current Time in UNIX
+    job->arrivalTimeStamp = *localtime(&job->arrivalTime);      // Full time struct
 }
--- modules/scheduler/jobCreationUtility.h
+++ modules/scheduler/jobCreationUtility.h
@@ -9,12 +9,13 @@
 #ifndef JOB_INIT_UTILITY_H_
 #define JOB_INIT_UTILITY_H_
 
+// Extracts args and copy thems into the jobArgs array of a Job
 void extractArgs(JobPtr job, char* cmdV[]);
-
+// Creates a job from given char* array, and returns a pointer
 JobPtr createJob(char* cmdV[]);
-
+// Updates the job's members based on given char*[]
 void populateJobFromBuffer(JobPtr job, char* cmdV[]);
-
+// Timestamp the current time, and added it to Job's startTime
 void timeStampNewJob(JobPtr job);
 
 #endif // JOB_INIT_UTILITY_H_
