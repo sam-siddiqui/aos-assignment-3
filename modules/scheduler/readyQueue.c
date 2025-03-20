c20afc90896391d5db8fe2422bcf226a97dba810
SamSidd <szs0421@auburn.edu>
2025-03-20 05:40:07 -0500

    Commented

--- modules/scheduler/readyQueue.c
+++ modules/scheduler/readyQueue.c
@@ -6,16 +6,19 @@ void increaseRQCount(int val) { atomic_fetch_add(&currCount, val); }
 int _getExpectedWaitingTime() {return atomic_load(&totalExpectedTime); }
 void decExpectedWaitingTime(int val) { atomic_fetch_sub(&totalExpectedTime, val); }
 void incExpectedWaitingTime(int val) { atomic_fetch_add(&totalExpectedTime, val); }
+
 int getExpectedWaitingTime() { 
+    // Based on the formula in the requirements.pdf
     if (isAJobRunning())
         return atomic_load(&totalExpectedTime) - (time(NULL) - RunningJob->startTime); 
-    
+    // Else get the actual value
     return _getExpectedWaitingTime();
 }
 
 void sortJobsBasedOnPolicy(u_int oldRQHead, u_int oldRQTail, enum Scheduling_Policy currSchedulePolicy) {
     u_int newRQHead = -1;
 
+    // X Macro to populate the switch statement using preprocessor and SCH_POLICY
     switch (currSchedulePolicy) {
         #define X(name, strrep, _) \
         case name: \
@@ -26,13 +29,13 @@ void sortJobsBasedOnPolicy(u_int oldRQHead, u_int oldRQTail, enum Scheduling_Pol
         default:
             break;
     }
-    rqTail = 0; 
+    rqTail = 0;                             // Reset the tail and head
     rqHead = newRQHead;
 }
 
 void addJobToRQHead(JobPtr job) {
     increaseRQCount(1);
-    readyQueue[rqHead] = *job;
+    readyQueue[rqHead] = *job;              // Add job's pointer to the queue
     safeIncRQHead();
 }
 
@@ -40,7 +43,7 @@ void clearJob(JobPtr job, int delete) {
     updateJobName(job, "");
     strcpy(job->jobId, "");
 
-    if (delete == DELETE_JOB && job->jobArgs != NULL) {
+    if (delete == DELETE_JOB && job->jobArgs != NULL) {         // If we're deleting
         for (int i = 0; i < job->nArgs; i++) {
             free(job->jobArgs[i]);  // Free each string
         }
@@ -52,7 +55,7 @@ void clearJob(JobPtr job, int delete) {
 
     job->priority = DEFAULT_PRIORITY;
     job->arrivalTime = 0;
-    memset(&job->arrivalTimeStamp, 0, sizeof(struct tm));
+    memset(&job->arrivalTimeStamp, 0, sizeof(struct tm));       // Zero out the struct
     job->executionTime = -1;
     job->CPUBurstTime = DEFAULT_BURST_TIME;
     job->startTime = 0;
--- modules/scheduler/readyQueue.h
+++ modules/scheduler/readyQueue.h
@@ -12,13 +12,13 @@ void increaseRQCount(int val);
 int getExpectedWaitingTime();
 void decExpectedWaitingTime(int val);
 void incExpectedWaitingTime(int val);
-
+// Sort the jobs and set the oldRQTail
 void sortJobsBasedOnPolicy(u_int oldRQHead, u_int oldRQTail, enum Scheduling_Policy currSchedulePolicy);
-
+// Actually adds the job to the queue
 void addJobToRQHead(JobPtr job);
-
+// Resets a job space for reuse
 void clearJob(JobPtr job, int delete);
-
+// Clears the readyQueue
 void clearReadyQueue(int delete);
 
 #endif // READY_QUEUE_H
