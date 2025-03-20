f56de76648d27e50ae13cd7ad9b1891263206c7c
SamSidd <szs0421@auburn.edu>
2025-03-20 05:56:03 -0500

    Commented

--- modules/cliParser/cliUtility.c
+++ modules/cliParser/cliUtility.c
@@ -3,25 +3,25 @@
 void copyCmdV(char* cmdV[]) {
     pthread_mutex_lock(&inpBufLock); 
     for (int i = 0; i < CMDV_NUM_ARGS; ++i) 
-    if(cmdV[i] != NULL) inputBufferArr[i] = strdup(cmdV[i]);  
+    if(cmdV[i] != NULL) inputBufferArr[i] = strdup(cmdV[i]);            // Make a copy of the char*
     pthread_mutex_unlock(&inpBufLock); 
 }
 
 int inputCleanup(char* buffer, char* cmdsV[]) {
     char* argToken;
     int token_idx = 0;
-    buffer[strcspn(buffer, "\r\n")] = 0;
+    buffer[strcspn(buffer, "\r\n")] = 0;                                // First split the string
     argToken = strtok(buffer, " ");
-    cmdsV[token_idx] = argToken;
+    cmdsV[token_idx] = argToken;                                        // First arg is first token
 
-    while (argToken != NULL && token_idx < CMDV_NUM_ARGS) {
-        token_idx++;
+    while (argToken != NULL && token_idx < CMDV_NUM_ARGS) {             // While not null
+        token_idx++;                                                    // Loop through the various space seperated input
         argToken = strtok(NULL, " ");
-        cmdsV[token_idx] = argToken;
+        cmdsV[token_idx] = argToken;                                    // And assign it to cmdsV
     };
 
     for (int i = token_idx; i < CMDV_NUM_ARGS; i++) {
-        cmdsV[i] = NULL;
+        cmdsV[i] = NULL;                                                // NULL out the remaining
     }
 
     return token_idx;   //cmdCount
--- modules/cliParser/cliUtility.h
+++ modules/cliParser/cliUtility.h
@@ -5,8 +5,9 @@
 
 #include "../../headers/text.h"
 #include "../../shared.h"
-
+// Copies args from CLI Parser to inputBufferArr
 void copyCmdV(char* cmdV[]);
+// Loops and extracts the inputs from the buffer into cmdsV
 int inputCleanup(char* buffer, char* cmdsV[]);
 
 #endif
