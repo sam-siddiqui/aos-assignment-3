8ce06ad4444e8f5e6bc1db14db97240072882e57
SamSidd <szs0421@auburn.edu>
2025-03-20 05:23:48 -0500

    Commented

--- modules/scheduler/schedulePolicy.c
+++ modules/scheduler/schedulePolicy.c
@@ -3,6 +3,7 @@
 int getJobAttribute(JobPtr job, char* policyName) {
     if (!isValidPolicy(policyName)) return job->queueNumber;
     
+    // X Macro to repeat the if statement for every policy in SCH_POLICY
     #define X(name, strrep, attribute) if (match(policyName, strrep)) return job->attribute;
     SCH_POLICY(X)
     #undef X
@@ -16,6 +17,7 @@ void sortQueue(u_int* currRQHead, char* policyName) {
     JobPtr tempJob;
     tempJob = (JobPtr)malloc(sizeof(struct Job));
 
+    // Insertion Sort of the Queue
     for (int i = 0; i < MAX_JOB_NUMBER; i++) {
         j = i;
         while (j > 0 && getJobAttribute(&readyQueue[j], policyName) < getJobAttribute(&readyQueue[j - 1], policyName)) {
@@ -27,6 +29,8 @@ void sortQueue(u_int* currRQHead, char* policyName) {
     }
 
     j = 0;
+
+    // Move head till the last job in the queue
     for (int i = 0; i < MAX_JOB_NUMBER; i++) {
         if (readyQueue[i].queueNumber != DEFAULT_QUEUE_NUMBER) j++;
     }
@@ -39,6 +43,7 @@ void sortQueue(u_int* currRQHead, char* policyName) {
 int changePolicy(char* givenPolicy) {
     if (!isValidPolicy(givenPolicy)) return INVALID_POLICY;
 
+    // X Macro to repeat the if line, as many policies there are
     #define X(name, strrep, _) if(match(givenPolicy, strrep)) currSchedulePolicy = name;
     SCH_POLICY(X)
     #undef X
--- modules/scheduler/schedulePolicy.h
+++ modules/scheduler/schedulePolicy.h
@@ -7,10 +7,12 @@
 #ifndef SCH_POLICY_H_
 #define SCH_POLICY_H_
 
+// Gets the policy related attribute as defined by SCH_POLICY
 int getJobAttribute(JobPtr job, char* policyName);
-
+// Sorts the RQ and sets the queues head and tail to last and first
 void sortQueue(u_int* currRQHead, char* policyName);
-
+// Uses isValidPolicy internally
+// Changes if success, returns failure if invalid policy
 int changePolicy(char* givenPolicy);
 
 #endif // SCH_POLICY_H_
